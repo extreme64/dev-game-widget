978183c30b46b1ef23c83a41186767698357ec80
Mastermind <adam.gicevic@gmail.com>
2023-06-08 01:39:29 +0200

    Badges html

--- badges.js
+++ badges.js
@@ -1,37 +1,55 @@
 
-// const <div id="awards-badges" class="awards awards--badge">
-//     <div class="awards__background sprite sprite1">
-//         <span class="awards__test"></span>
-//         <span class="awards__download">
-//             <button class="awards__button">Save award</button>
-//         </span>
-//     </div>
-// </div>
+const Badges = (function () {
 
-const badgesLevels = `<div id="badges-levels" class="badges badges--level">
-    <div class="badges__background sprite sprite1">
-        <span class="badges__test"></span>
-        <span class="badges__download">
-            <button class="badges__button">Save badge</button>
-        </span>
-    </div>
-</div>`;
+    const level = `<div id="badges-levels" class="badges badges--level">
+        <div class="badges__background sprite sprite1">
+            <span class="badges__test"></span>
+            <span class="badges__download">
+                <button class="badges__button">Save badge</button>
+            </span>
+        </div>
+    </div>`;
 
-const badgesSuggestion = `   <div id="badges-suggestion" class="badges badges--suggestion">
-    <div class="badges__background sprite sprite1">
-        <span class="badges__test"></span>
-        <span class="badges__download">
-            <button class="badges__button">Save badge</button>
-        </span>
-    </div>
-</div>`;
+    const suggestion = `<div id="badges-suggestion" class="badges badges--suggestion">
+        <div class="badges__background sprite sprite1">
+            <span class="badges__test"></span>
+            <span class="badges__download">
+                <button class="badges__button">Save badge</button>
+            </span>
+        </div>
+    </div>`;
 
-const badgesUnlocked = `<div id="badges-unlocked" class="badges badges--unlocked">
-    <div class="badges__background sprite sprite1">
-        <span class="badges__test"></span>
-        <span class="badges__download">
-            <button class="badges__button">Save badge</button>
-        </span>
-    </div>
-</div>`;
- 
+    const unlocked = `<div id="badges-unlocked" class="badges badges--unlocked">
+        <div class="badges__background sprite sprite1">
+            <span class="badges__test"></span>
+            <span class="badges__download">
+                <button class="badges__button">Save badge</button>
+            </span>
+        </div>
+    </div>`;
+
+
+
+    const showAward = ((awardID) => {
+        const awardWrapEl = document.querySelector("#badges-levels")
+        const sprite = document.querySelector("#badges-levels .sprite")
+        awardWrapEl.style.display = "flex"
+        sprite.style.animationPlayState = "running";
+
+        // Self hide
+        setTimeout(() => {
+            if (typeof awardWrapEl === 'undefined' || typeof sprite === 'undefined') return
+            awardWrapEl.style.display = 'none';
+            sprite.style.animationPlayState = "paused";
+        }, 4000);
+    })
+
+
+
+    return {
+        showAward: showAward,
+        level: level,
+        suggestion: suggestion,
+        unlocked: unlocked
+    }
+})()
