863de4d7844d58afc5f1a923b1a3495327162808
Mastermind <adam.gicevic@gmail.com>
2023-08-31 00:44:25 +0200

    Log in form. Log in remote to the server.

--- components/user/user.js
+++ components/user/user.js
@@ -1,18 +1,97 @@
 const user = (function () {
     
+    let thisNode
+    let logInForm
+    let logInStatus
+
     const html = `
-        <div class="widget__avatar">
-            ✨✨✨
-        </div>
-        <!-- user wrap -->
-        <div class="widget__user">
-            <a href="https://github.com/extreme64"  title="Visit profile" target="_blank">
-            👦 Mast_G
-            </a>
+        <div data-ui="user">
+            <div class="widget__avatar">
+                ✨✨✨
+            </div>
+            <!-- user wrap -->
+            <div class="widget__user">
+                <div>
+                    <form name="loginForm">
+                        <input type="text" name="email"/>
+                        <input type="password" name="bsr"/>
+                        <button name="sbmLogInReq">Log In</button>
+                        <button>Cancel</button>
+                    </form>
+                </div>
+                <div>
+                    <a data-ui="profileLink" href="http://localhost:8000/dash"  title="Visit profile" target="_blank">User name</a>
+                    <a data-ui="logOutLink" href="http://localhost:8000/api/remote-logout"  title="Log out">🔌</a>
+                </div>
+            </div>
         </div>`
 
+    function onReady(parent){
+        thisNode = parent
+        logInForm = parent.querySelector('[name="loginForm"]');
+        handleUserInfoDisplay(localStorage.getItem('name'))
+        logInForm.addEventListener('submit', (e) => {
+            logIn(e)
+        }) 
+    }
+
+    function handleFormDisplay(status) {
+        if (status) {
+            logInForm.style.display = "none"
+        }
+    }
+
+    function handleUserInfoDisplay(name="Profile name,...", email='') {
+        thisNode.querySelector('[data-ui="profileLink"]').innerHTML = name
+    }
+
+
+    const logIn = (async (e) => {
+        e.preventDefault();
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        try{
+            let requestBodydata = {}
+            e.target.childNodes.forEach((node) =>  {
+                if(node.name === "email") {
+                    requestBodydata['email'] = node.value
+                }
+                if (node.name === "bsr") {
+                    requestBodydata['password'] = node.value
+                }
+            })
+
+            let response = await fetch(`http://localhost:8000/api/remote-login?email=${requestBodydata['email']}&password=${requestBodydata['password']}`, {
+                method: 'POST',
+                headers: headers
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                localStorage.setItem('rlgin', data.token)
+                localStorage.setItem('name', data.user.name)
+                localStorage.setItem('email', data.user.email)
+
+                logInStatus = true
+                
+                handleFormDisplay(logInStatus)
+                handleUserInfoDisplay(data.user.name, data.user.email)
+            } else {
+                console.error('Error logging in!', response);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    })
+
     return {
-        html
+        html,
+        onReady,
+        logIn,
+        handleFormDisplay,
+        logInStatus
     }
 
 })()
