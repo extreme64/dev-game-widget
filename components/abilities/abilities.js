f2cc4e3031ac1fbd9db92cb03454ee93d5f4417d
Mastermind <adam.gicevic@gmail.com>
2023-09-02 15:24:08 +0200

    Abilitis work with server and localStrg.

--- components/abilities/abilities.js
+++ components/abilities/abilities.js
@@ -5,19 +5,19 @@ const AbilitiesModule = (function () {
     const html = `
         <div class="widget__abilities">
             <ul data-abilities class="widget__abilities-list">
-                <li data-id="0" class="widget__abilities-item" title=""></li>
-                <li data-id="1" class="widget__abilities-item" title=""></li>
-                <li data-id="2" class="widget__abilities-item" title=""></li>
-                <li data-id="3" class="widget__abilities-item" title=""></li>
-                <li data-id="4" class="widget__abilities-item" title=""></li>
-                <li data-id="5" class="widget__abilities-item" title=""></li>
-                <li data-id="6" class="widget__abilities-item" title=""></li>
+                <li class="widget__abilities-item"></li>
+                <li class="widget__abilities-item"></li>
+                <li class="widget__abilities-item"></li>
+                <li class="widget__abilities-item"></li>
+                <li class="widget__abilities-item"></li>
+                <li class="widget__abilities-item"></li>
+                <li class="widget__abilities-item"></li>
             </ul>
         </div>`;
 
-    const abilities = [
+    let abilities = [
         {
-            id: 0,
+            id: 1,
             title: "I had a vision!",
             desc: "Plan before code, thing before write.",
             icon: "🔮",
@@ -25,7 +25,7 @@ const AbilitiesModule = (function () {
             isUsed: false
         },
         {
-            id: 1,
+            id: 2,
             title: "I chat with bud GPT!",
             desc: "It is a good tool use it to a full extent.",
             icon: "🔎",
@@ -33,7 +33,7 @@ const AbilitiesModule = (function () {
             isUsed: false
         },
         {
-            id: 2,
+            id: 3,
             title: "I been monitoring!",
             desc: "Abserve and note, seeing clearly is important.",
             icon: "📈",
@@ -41,7 +41,7 @@ const AbilitiesModule = (function () {
             isUsed: false
         },
         {
-            id: 3,
+            id: 4,
             title: "I removed/fixed big bugs!",
             desc: "It is hard, but there is no way around. Longer they linger, harder they become.",
             icon: "🔫",
@@ -49,7 +49,7 @@ const AbilitiesModule = (function () {
             isUsed: false
         },
         {
-            id: 4,
+            id: 5,
             title: "I have less to worry now!",
             desc: "Less code is beautiful.",
             icon: "✂️",
@@ -57,7 +57,7 @@ const AbilitiesModule = (function () {
             isUsed: false
         },
         {
-            id: 5,
+            id: 6,
             title: "I refactor many!",
             desc: "It is an art form, practise it daily.",
             icon: "💡",
@@ -65,7 +65,7 @@ const AbilitiesModule = (function () {
             isUsed: false
         },
         {
-            id: 6,
+            id: 7,
             title: "I did epic code!",
             desc: "Epic feature are nice milestones to be proud of.",
             icon: "💎",
@@ -74,8 +74,20 @@ const AbilitiesModule = (function () {
         }
     ];
 
-    function init(abilitesChildren) {
+    function onReady(abilitesChildren) {
+        
         setNodesData(abilitesChildren);
+        
+        let abilsStr = localStorage.getItem(QUEST_ABILITIES_LSKEY)
+        abilsSavedIds = abilsStr.split(';');
+        abilsSavedIds.forEach((abil, i) => {
+            let item = undefined
+            item = document.querySelector(`.widget__abilities [data-id="${abil}"]`)
+            if (item !== null && item !== undefined) {
+                item.classList.add('used');
+            }
+        })
+        
         Events.setEventListeners("click", abilitesChildren, abilityClick);
     }
 
@@ -87,33 +99,59 @@ const AbilitiesModule = (function () {
         })
     }
 
-    function abilityClick(abilityId, event, listener) {
+    async function abilityClick(abilityId, event, listener) {
         if (typeof abilities === 'undefined') return;
+
+        const projectId = 1;
 
         // If the ability is already used, return
         if (abilities[abilityId].isUsed) return;
 
-        // Remove the listener for this ability
-        event.currentTarget.removeEventListener('click', listener);
+        let questId = localStorage.getItem(QUEST_ID_LSKEY)
+        let token = localStorage.getItem('rlgin')
+        
+        await fetch(`http://localhost:8000/api/project/${projectId}/quests/id/${questId}/ability/${abilityId}`, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Events sent successfully:', data);
 
-        // Style btn. as 'spent'
-        event.currentTarget.classList.add('used');
+                // Remove the listener for this ability
+                event.target.removeEventListener('click', listener);
 
-        // Update the isUsed status of the ability
-        abilities[abilityId].isUsed = true;
+                // Style btn. as 'spent'
+                event.target.classList.add('used');
+
+                // Update the isUsed status of the ability
+                abilities[abilityId].isUsed = true;
+
+                // FIXME: update score centrilized else where
+                const abilitiesToUpdateTo = data.message.abilitiesUsed
+                const scoreToUpdateTo = data.message.newScore
+                const levelToUpdateTo = data.message.newLevel
+                localStorage.setItem(QUEST_ABILITIES_LSKEY, abilitiesToUpdateTo)
+                localStorage.setItem(QUEST_SCORE_LSKEY, scoreToUpdateTo)
+                localStorage.setItem(QUEST_LEVEL_LSKEY, levelToUpdateTo)
+            })
+            .catch(error => {
+                console.error('Error sending events:', error);
+            });
 
         // Update score
-        currentScore += abilities[abilityId].score;
+        // currentScore += abilities[abilityId].score;
 
-        // Show the updated score
-        console.log(`[${abilityId}] Current score: currentScore + ${abilities[abilityId].score}`);
     }
 
     return {
         abilityClick: abilityClick,
         setNodesData: setNodesData,
         html: html,
-        init: init
+        onReady: onReady
     };
     
 })();
