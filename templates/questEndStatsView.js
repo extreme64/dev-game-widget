1a5f2179973b854fc95f2da4566aef281fe973df
Mastermind <adam.gicevic@gmail.com>
2023-12-19 23:31:35 +0100

    End stats card.

--- templates/questEndStatsView.js
+++ templates/questEndStatsView.js
@@ -3,46 +3,124 @@ const QuestEndStatsView = (() => {
 
     // FIXME: Stop apps CSS impact our extension markup, add all declarations that are important to us.
     const QuestEndStatsHtml = `
-        <section id="quest-end-stats" class="quest-end-stats">
-
-            <div>
-                <!-- Level -->
-            </div>
-            
-            <div>
-                <!-- Avatar -->
-            </div>
-            
+        <section id="quest-end-stats" class="quest-end-stats hide">
             <div class="end-stats__panels-wrap">
-
-                <div class="end-stats__card">
-                    <!-- stats wrap -->
-                    <!-- ${stats.html} -->
-            
-                    <!-- abilities wrap -->
-                    <!-- ${AbilitiesModule.html} -->
-                    <div class="end-stats__card__abilities widget__abilities">
-                        <ul data-abilities class="widget__abilities-list">
-                            <li class="widget__abilities-item"></li>
-                            <li class="widget__abilities-item"></li>
-                            <li class="widget__abilities-item"></li>
-                            <li class="widget__abilities-item"></li>
-                            <li class="widget__abilities-item"></li>
-                            <li class="widget__abilities-item"></li>
-                            <li class="widget__abilities-item"></li>
+                
+                <span class="end-stats__illustration">
+                    <img class="illustration_img" 
+                        src="" />
+                </span>
+                <main class="end-stats__card-main">
+                    <div class="card-main__avatar">
+                        <img class="avatar__img" 
+                        src="" />
+                    </div>
+                        
+                    <div class="card-main__level">
+                        <b class="level__text">
+                            7
+                        </b>
+                    </div>
+    
+                    <div class="card-main__status">
+                        <i>QUEST COMPLITED</i>
+                    </div>
+                        
+                    <div class="card-main__score">
+                        <b class="score__text">
+                            12222
+                        </b>
+                    </div>
+                </main>
+                        
+                <aside class="end-stats__card-details">
+                    <div class="card-details__abilities">
+                        <ul data-abilities class="abilities__list">
+                            <li class="abilities-item">[ + ]</li>
+                            <li class="abilities-item">[ + ]</li>
+                            <li class="abilities-item">[ + ]</li>
+                            <li class="abilities-item">[ + ]</li>
+                            <li class="abilities-item">[ + ]</li>
+                            <li class="abilities-item">[ + ]</li>
+                            <li class="abilities-item">[ + ]</li>
                         </ul>
                     </div>
-                </div>
+                </aside>
 
-                <div class="end-stats__stat-details">
-            
-                </div>
+                <span 
+                    data-ui="close-btn" 
+                    class="end-stats__ui-close"></span>
+
             </div>
-           
         </section>`;
 
+        /**
+         * Get data and prep it into the card layout
+         *
+         * @return [type]
+         * 
+         */
+        async function initCard(questWinStatus) {
+            let contextEl = document.querySelector('#quest-end-stats')
+            // Get quest info
+            const projectAttrs = await Project.getAttributes();
+            setCard(contextEl, projectAttrs)
+            setUser(contextEl, projectAttrs)
+            setText(contextEl, { 
+                score: currentScore, 
+                level: currentLevel, 
+                statusText: (questWinStatus) ? '+ QUEST COMPLITED +' : '- QUEST FAILED -' 
+            });
+
+            showCard();
+        }
+        
+        /**
+         * Display card
+         *
+         * @return [type]
+         * 
+         */
+        function showCard() {
+
+            let card = document.querySelector('.quest-end-stats')
+            
+            if(!card){
+                console.error('Element with ID "quest-end-stats" not found.');
+            }
+
+            card.classList.toggle('hide')
+            card.classList.toggle('show')
+        }
+
+
+        // Set graphics for project
+        function setCard(contextEl, urls) {
+            let illustrationImg = contextEl.querySelector('.illustration_img')
+            illustrationImg.src = urls.background_id
+        }
+
+        function setUser(contextEl, urls) {
+            let avatarImg = contextEl.querySelector('.avatar__img')
+            avatarImg.src = urls.avatar_id
+        }
+
+        // Set text
+        function setText(contextEl, texts) {
+
+            let levelTxt = contextEl.querySelector('.level__text')
+            levelTxt.innerHTML = texts.level
+
+            let scoreTxt = contextEl.querySelector('.score__text')
+            scoreTxt.innerHTML = texts.score
+
+            let statusTxt = contextEl.querySelector('.card-main__status')
+            statusTxt.innerHTML = texts.statusText
+        }
+
     return {
-        layout: QuestEndStatsHtml
+        layout: QuestEndStatsHtml,
+        initCard: initCard
     }
 
 })()
